21f214281c7369c0c47d12a8329508d6c1efd1a2
CCStage <stage@clipconsultants.nl>
2026-08-26 10:05:20 +0200

    Simple Calcualtor en Password validator gemaakt

---
+++ CCDemo.Data.Tests.Unit/PasswordValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿namespace CCDemo.Data.Tests.Unit
+{
+    public class PasswordValidatorTests
+    {
+        private readonly PasswordValidator _sut = new();
+
+        [Fact]
+        public void HasUpperCase_WhenContainsUpperCase()
+        {
+            string password = "Password";
+
+            bool result = _sut.HasUpperCase(password);
+
+            Assert.True(result);
+        }
+        //twee tests
+
+        [Fact]
+        public void HasLowerCase_WhenContainsLowerCase()
+        {
+            string password = "PASSWORDa";
+
+            bool result = _sut.HasLowerCase(password);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void HasDigit_WhenContainsDigit()
+        {
+            string password = "Password1";
+
+            bool result = _sut.HasDigit(password);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void HasSpecialCharacter_WhenContainsSpecialCharacter()
+        {
+            string password = "Password!";
+
+            bool result = _sut.HasSpecialCharacter(password);
+
+            Assert.True(result);
+        }
+    }
+}
--- CCDemo.Data.Tests.Unit/SimpleCalculatorTests.cs
+++ CCDemo.Data.Tests.Unit/SimpleCalculatorTests.cs
@@ -2,6 +2,8 @@
 {
     public class SimpleCalculatorTests
     {
+
+
         private readonly SimpleCalculator _sut = new();
 
         [Fact]
@@ -17,5 +19,70 @@
             // Assert
             Assert.Equal(expectedOutput, output);
         }
+
+        [Fact]
+        public void SubtractThree_SubtractsThreeFromTheInput()
+        {
+            int input = 4;
+            int expectedOutput = 1;
+
+            int output = _sut.SubtractThree(input);
+
+            Assert.Equal(expectedOutput, output);
+        }
+
+        [Fact]
+        public void MultiplyByTwo_MultipliesTheInputByTwo()
+        {
+            int input = 4;
+            int expectedOutput = 8;
+
+            int output = _sut.MultiplyByTwo(input);
+
+            Assert.Equal(expectedOutput, output);
+        }
+
+        [Fact]
+        public void DivideByTwo_DividesTheInputByTwo()
+        {
+            int input = 4;
+            int expectedOutput = 2;
+
+            int output = _sut.DivideByTwo(input);
+
+            Assert.Equal(expectedOutput, output);
+        }
+
+        [Fact]
+        public void DivideByTwo_ThrowsArgumentException_WhenInputIsZero()
+        {
+            int input = 0;
+
+            Assert.Throws<ArgumentException>(() => _sut.DivideByTwo(input));
+        }
+        [Fact]
+        public void IsEven_ReturnsTrue_WhenInputIsEven()
+        {
+            int input = 4;
+
+            bool output = _sut.IsEven(input);
+
+            Assert.True(output);
+        }
+
+        [Fact]
+        public void IsEven_ReturnsFalse_WhenInputIsOdd()
+        {
+            int input = 1;
+
+            bool output = _sut.IsEven(input);
+
+            Assert.False(output);
+        }
     }
+   
 }
+
+    
+
+
